bd43bcd59baacc4acc5c69e306aa5fadec88ddc4
Yusef-H <yusefhalabi456@gmail.com>
2023-06-11 13:34:07 +0300

    setup project skeleton and classes

--- src/Question.java
+++ src/Question.java
@@ -1,4 +1,12 @@
+import java.io.Serializable;
 
-public class Question {
+public class Question implements Serializable {
+	String question;
+	String[] incorrectAnswers;
+	String correctAnswer;
+	
+	public boolean isCorrectAnswer(String answer) {
+		return answer.equals(correctAnswer);
+	}
 
 }
