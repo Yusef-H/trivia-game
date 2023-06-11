411ed553eb76f70521346a54caf5aeaffede593b
Yusef-H <yusefhalabi456@gmail.com>
2023-06-11 14:28:19 +0300

    start implementing server - fetch questions from api

--- src/Question.java
+++ src/Question.java
@@ -5,6 +5,15 @@ public class Question implements Serializable {
 	String[] incorrectAnswers;
 	String correctAnswer;
 	
+	
+	public Question(String question, String[] incorrectAnswers, String correctAnswer) {
+		super();
+		this.question = question;
+		this.incorrectAnswers = incorrectAnswers;
+		this.correctAnswer = correctAnswer;
+	}
+
+
 	public boolean isCorrectAnswer(String answer) {
 		return answer.equals(correctAnswer);
 	}
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,4 +1,23 @@
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class ServerThread {
+public class ServerThread extends Thread{
+	ArrayList<Question> questions;
+	private Socket s = null;
+	
+	public ServerThread(Socket socket, ArrayList<Question> questions) {
+		this.s = socket;
+		this.questions = questions;
+	}
+	
+	@Override
+	public void run() {
+		super.run();
+		try {
+			
+		} catch(Exception e) {
+			
+		}
+	}
 
 }
